e00728fe1532829860ae32c08c2313d70bc86698
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-02-26 10:49:24 +0000

    first commit

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -5,7 +5,7 @@ function config() {
             tiles: 'https://raw.githubusercontent.com/acycliq/data_dump/master/img/ca1/262144px/{z}/{y}/{x}.jpg',
             cellData: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/cellData',
             geneData: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/geneData',
-            cellCoords: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/cellCoords',
+            cellCoords: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/cellCoords_dip',
             class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
         }
 }
