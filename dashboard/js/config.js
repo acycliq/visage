2bc030a72c5da8067a8a7f27cc0d7db954db5fae
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-04-17 13:32:44 +0100

    cellCoords replaced by cellBoundaries

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -4,8 +4,8 @@ function config() {
             imageSize: [262144, 188660],
             tiles: 'https://raw.githubusercontent.com/acycliq/data_dump/master/img/ca1/262144px/{z}/{y}/{x}.jpg',
             cellData: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/cellData',
-            geneData: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/geneData?ref=main',
-            cellCoords: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/cellCoords',
+            geneData: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/geneData',
+            cellBoundaries: 'https://api.github.com/repos/acycliq/ca1/contents/dashboard/data/cellBoundaries',
             class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
         }
 }
--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -159,7 +159,7 @@ function run() {
     console.log('app starts');
     configSettings = config();
 
-    fetcher([configSettings.cellData, configSettings.geneData, configSettings.cellCoords]).then(
+    fetcher([configSettings.cellData, configSettings.geneData, configSettings.cellBoundaries]).then(
         result => make_package(result),
         error => alert(error) // doesn't run
     );
