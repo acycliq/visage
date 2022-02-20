878d5f0c7690c89892e84ba01157320507ad4e5a
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2022-02-20 22:54:40 +0000

    minor edit

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -6,9 +6,9 @@ function config() {
             cellData: 'https://www.googleapis.com/storage/v1/b/ca1-data/o/cellData%2FcellData.tsv',
             geneData: 'https://www.googleapis.com/storage/v1/b/ca1-data/o/geneData%2FgeneData.tsv',
             cellBoundaries: 'https://www.googleapis.com/storage/v1/b/ca1-data/o/cellBoundaries%2FcellBoundaries.tsv',
-            class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
+            class_name_separator: '.' //Classes/subclasees etc are delimited by a dot
         }
 }
 
-// In google cloud storage, the objects must be encoded. That means in the links above pointing to googleapis.com, all / after /o/ have been replaced by %2F
-// There is a helper function by the encode(url) function inside index.js that can be used to do the encoding
+// In google cloud storage, the urls must be encoded. That means in the links above pointing to googleapis.com, all / after /o/ have been replaced by %2F
+// Hence the encoded form of 'https://www.googleapis.com/storage/v1/b/ca1-data/o/cellData/cellData.tsv' is 'https://www.googleapis.com/storage/v1/b/ca1-data/o/cellData%2FcellData.tsv',
