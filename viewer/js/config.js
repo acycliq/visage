d56ebca2250814d0ea6ba9b40f9b5a3854075668
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2023-06-03 23:14:23 +0100

    added multiple layers (baselayers)

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -13,7 +13,11 @@ function config() {
     return {
             roi: {"x0": 0, "x1": 7602, "y0": 0, "y1": 5471 },
             zoomLevels: 10, // maximum zoom levels. Leave that at 10.
-            tiles: 'https://storage.googleapis.com/ca1-data/img/262144px/{z}/{y}/{x}.jpg',
+            layers: {'dapi': 'https://storage.googleapis.com/ca1-data/img/262144px/{z}/{y}/{x}.jpg',
+                     'dapi_shuffled': 'https://storage.googleapis.com/ca1-data/img/262144px/{z}/{x}/{y}.jpg',
+                     'empty': '',
+                     'world_shuffled': 'http://{s}.tile.osm.org/{z}/{y}/{x}.png',
+                     'world': 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'},
             cellData: {mediaLink: '../data/cellData.tsv', size: "2179232"},
             geneData: {mediaLink: '../data/geneData.tsv', size: "9630856"},
             cellBoundaries: {mediaLink: '../data/cellBoundaries.tsv', size: "1306209"},
